620248546fd8ed0ef1cdd93212ce2b139bedcc7b
Matt Carver <mattcarver.wd@gmail.com>
2020-11-17 14:10:25 -0800

    adding js

---
+++ script.js
@@ -0,0 +1,33 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+
+//Array of objects, with name, first and last and then a money value.
+
+//Intent: Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    console.log(newUser);
+    //results.name.first.value?
+}
+
+//Fetch is asyncronous, so we have to wait for it to finish, and it returns a promise when it's finished, and we have to put await in front of it.
